a0030a178a0781b95e5bdcf9869c820397acc865
Gaku <rxf10242@gmail.com>
2026-08-08 23:03:59 +0900

    Tablacus DISK ROM Litev0.7.4.1から引き継ぎ

---
+++ msxcat.bat
@@ -0,0 +1,4 @@
+rem Tablacus DISK RAM Lite (‘‚«ž‚Ý‘Î‰ž”Å) ‚Ì—á
+copy /b DISKRAM.BIN + FD720KB.DSK MSXRAM.ROM
+copy /b DISKRAM64K.BIN + FD720KB.DSK MSX64KBAS.ROM
+copy /b DISKRAM64K.BIN + dos2formsx.dsk MSXRAM64K.ROM
